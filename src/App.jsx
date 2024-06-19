26567cdeae40aaf770f688573c49f57bdda9622d
Elijah Adams <emadams92@gmail.com>
2024-06-19 10:25:00 -0700

    Add MUI and clean up uneeded css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import './App.css';
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
 import OrderPage from './pages/OrderPage';
 import AboutUs from './pages/AboutUs';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
   const router = createBrowserRouter(
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 
 const HomePage = () => {
 
-
   return (
-    <div>HomePage</div>
+    <>
+    <Button variant="contained">Order</Button>
+    </>
   )
 }
 
