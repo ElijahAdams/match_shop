3f5a80738cf4e08763f108c02d79ffb383b05106
Elijah Adams <emadams92@gmail.com>
2024-06-19 09:31:52 -0700

    Add react router with Mainlayout and routes

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+const MainLayout = () => {
+  return (
+    <>
+    <h1>Main Layout</h1>
+    <Outlet />
+    </>
+  )
+}
+
+export default MainLayout
