883d89a1db5dfe72cb95b6502ed52427d36f402b
Elijah Adams <emadams92@gmail.com>
2024-07-08 20:42:01 -0700

    set states for customizations

--- src/components/Customizations.jsx
+++ src/components/Customizations.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import {React, useEffect, useState} from 'react'
 import {
+  Flex,
   RadioGroup,
   Radio,
   Slider,
   SliderTrack,
   SliderFilledTrack, 
   SliderThumb,
+  SliderMark,
   Stack,
-  Switch
+  Switch,
+  Spacer
 } from '@chakra-ui/react'
 const Customizations = ({menuItemConfigs}) => {
   /**
@@ -37,23 +40,70 @@ const Customizations = ({menuItemConfigs}) => {
    *   off: ""
    * }
    */
+  const [customizations, setCustomizations] = useState({});
+  // // TODO: set default states based on menuItemConfigs
 
+  const handleChange = ({target}) => {
+    setCustomizations(customizations => ({
+      ...customizations,
+      [target.id]: target.value
+    }));
+  }
+
+  const handleChangeDirectValue = (value, label) => {
+    setCustomizations(customizations => { 
+      return {
+        ...customizations,
+        [label]: value
+      }
+    });
+  }
+
+  useEffect(() => {
+    // TODO: return customizations to parent component
+    console.log(customizations);
+  }, [customizations]);
+  
   return (
     <>
       {menuItemConfigs.map(menuItemConfig => (
         <Stack key={menuItemConfig.label}>
-
-
+          <Spacer />  
           { menuItemConfig.componentType === "toggle" && 
             <>
               <h3>{menuItemConfig.label}</h3>
-              <Switch size='md' />
+              <Flex gap="2">
+                <span>hot</span>
+                <Switch id={menuItemConfig.label} size="md" onChange={handleChange}/>
+                <span>cold</span>
+              </Flex>
             </>
-          }   
+          } 
+          <Spacer />  
           { menuItemConfig.componentType === "slider" && 
             <>
               <h3>{menuItemConfig.label}</h3>
-              <Slider aria-label='slider-ex-1' defaultValue={30}>
+              <Slider id={menuItemConfig.label} aria-label="slider-ex-6" onChange={(value) => handleChangeDirectValue(value, menuItemConfig.label)}>
+                <SliderMark value={25} >
+                  25%
+                </SliderMark>
+                <SliderMark value={50} >
+                  50%
+                </SliderMark>
+                <SliderMark value={75}>
+                  75%
+                </SliderMark>
+                <SliderMark
+                  value="50"
+                  textAlign="center"
+                  bg="blue.500"
+                  color="white"
+                  mt="-10"
+                  ml="-5"
+                  w="12"
+                >
+                  {customizations[menuItemConfig.label]}%
+                </SliderMark>
                 <SliderTrack>
                   <SliderFilledTrack />
                 </SliderTrack>
@@ -61,14 +111,18 @@ const Customizations = ({menuItemConfigs}) => {
               </Slider>
             </>
           }   
+          <Spacer />  
           { menuItemConfig.componentType === "radio" && 
             <>
               <h3>{menuItemConfig.label}</h3>
-              <RadioGroup >
-                <Stack direction='row'>
-                  <Radio value='1'>First</Radio>
-                  <Radio value='2'>Second</Radio>
-                  <Radio value='3'>Third</Radio>
+              <RadioGroup 
+                id={menuItemConfig.label} 
+                defaultValue={menuItemConfig.componentDetails.options[0]} 
+                onChange={(value) => handleChangeDirectValue(value, menuItemConfig.label)}>
+                <Stack direction="row">
+                  {menuItemConfig.componentDetails.options.map(option => (
+                    <Radio key={option} value={option}>{option}</Radio>
+                  ))}
                 </Stack>
               </RadioGroup>
             </>
