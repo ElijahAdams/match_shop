dd1339ebd18f722782d4a74bd169f861a824133e
Elijah Adams <emadams92@gmail.com>
2024-07-22 17:00:01 -0700

    add layout for checkout integration

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,6 +6,7 @@ import {
   Card, 
   CardBody, 
   Fade, 
+  Flex,
   Heading, 
   HStack, 
   Icon, 
@@ -13,7 +14,6 @@ import {
   Image, Stack, Text } from '@chakra-ui/react';
 import { FaTrash } from "react-icons/fa";
 import { deleteItem, updateCartItem } from '../utils/CartUtil';
-import { indexOf } from 'lodash';
 const Cart = () => {
   const {cart, setCart } = useContext(CartContext);
 
@@ -34,49 +34,57 @@ const Cart = () => {
   return (
     <>
     <Fade in={true} transition={{enter: { duration: 1.5 }}}>
-      {cart.map((c, index) => (
-        <Card
-          key={c.name + index}
-          direction="row"
-          overflow="hidden"
-          variant="outline"
-        >
-          <Image
-            objectFit="cover"
-            maxW={{ base: '125px', sm: '200px' }}
-            src={c.photoUrl}
-            alt={c.alt}
-          />
-          <Stack>
-            <CardBody>
-              <Heading size="md">{c.name}</Heading>
-              <Text py="2" color="blue">
-                ${c.price}
-              </Text>
-              <Box py="2">
-                {Object.entries(c.details).map(([key, value]) => (  
-                  <Text key={c.name + key} mr="5px">{key} : {value}</Text>
-                ))}
-              </Box>
-              <HStack maxW='320px'>
-                { c.count === 1 ?
-                <IconButton 
-                    variant='ghost'
-                    colorScheme='red'
-                    size="md"
-                    onClick={() => deleteCartItem(c)}
-                    icon={
-                      <Icon as={FaTrash} boxSize={6}/>
-                  }/> : 
-                  <Button onClick={() => decrementItemCount(c)}>-</Button>
-                }
-                <input className="countInput" value={c.count} readOnly onChange={() => {}}/>
-                <Button onClick={() => incrementItemCount(c)} isDisabled={c.count >= 25}>+</Button>
-              </HStack>
-            </CardBody>
-          </Stack>
-        </Card>
-      ))}
+      <Flex pl="30px" pr="30px" flexDirection={{base: "column-reverse", md: "row"}}>
+        <Box w={{base: "100%", md: "50%"}}>
+          Checkout Integration
+        </Box>
+        <Box w={{base: "100%", md: "50%"}}>
+          {cart.map((c, index) => (
+            <Card
+              mb="15px"
+              key={c.name + index}
+              direction="row"
+              overflow="hidden"
+              variant="outline"
+            >
+              <Image
+                objectFit="cover"
+                maxW={{ base: '125px', sm: '200px' }}
+                src={c.photoUrl}
+                alt={c.alt}
+              />
+              <Stack>
+                <CardBody>
+                  <Heading size="md">{c.name}</Heading>
+                  <Text py="2" color="blue">
+                    ${c.price}
+                  </Text>
+                  <Box py="2">
+                    {Object.entries(c.details).map(([key, value]) => (  
+                      <Text key={c.name + key} mr="5px">{key} : {value}</Text>
+                    ))}
+                  </Box>
+                  <HStack maxW='320px'>
+                    { c.count === 1 ?
+                    <IconButton 
+                        variant='ghost'
+                        colorScheme='red'
+                        size="md"
+                        onClick={() => deleteCartItem(c)}
+                        icon={
+                          <Icon as={FaTrash} boxSize={6}/>
+                      }/> : 
+                      <Button onClick={() => decrementItemCount(c)}>-</Button>
+                    }
+                    <input className="countInput" value={c.count} readOnly onChange={() => {}}/>
+                    <Button onClick={() => incrementItemCount(c)} isDisabled={c.count >= 25}>+</Button>
+                  </HStack>
+                </CardBody>
+              </Stack>
+            </Card>
+          ))}
+        </Box>
+      </Flex>
     </Fade>
     </>
   )
