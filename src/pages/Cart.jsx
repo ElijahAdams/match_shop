1ad6a4c868486847b94eb3123c962c0d52e9cbf1
Elijah Adams <emadams92@gmail.com>
2024-07-30 20:59:43 -0700

    Switch to payment elements for stripe

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,12 @@
-import { React, useCallback, useContext } from 'react'
+import { React, useCallback, useEffect, useContext, useState } from 'react'
 import { CartContext } from '../App';
+import StripeCheckout from '../components/StripeCheckout';
 import { 
   Box, 
   Button,
   Card, 
   CardBody, 
+  CircularProgress,
   Fade, 
   Flex,
   Heading, 
@@ -16,8 +18,7 @@ import { FaTrash } from "react-icons/fa";
 import { deleteItem, updateCartItem } from '../utils/CartUtil';
 import {loadStripe} from '@stripe/stripe-js';
 import {
-  EmbeddedCheckoutProvider,
-  EmbeddedCheckout
+  Elements,
 } from '@stripe/react-stripe-js';
 import { useNavigate } from 'react-router-dom';
 
@@ -27,24 +28,29 @@ const stripePromise = loadStripe(env.VITE_STRIPE_API_KEY);
 const Cart = () => {
   const {cart, setCart } = useContext(CartContext);
   const navigate = useNavigate();
-  const fetchClientSecret = useCallback(() => {
-    // lineItems
-    const lineItems =  cart.map(cartItem => {
-      return {price: cartItem.priceId, quantity: cartItem.count}
-    });
 
-    // Create a Checkout Session
-    return fetch("/api/create-checkout-session", {
-        method: "POST",
-        body: JSON.stringify({lineItems}),
-        headers: {
-          "Content-Type": "application/json",
-        }
-    }).then((res) => res.json())
-      .then((data) => data.clientSecret);
+  const [clientSecret, setClientSecret] = useState("");
+  useEffect(() => {
+    const items =  cart.map(cartItem => {
+      return {id: cartItem.productId, count: cartItem.count}
+    });
+    // Create PaymentIntent as soon as the page loads
+    fetch("/api/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
   }, []);
+
+  const appearance = {
+    theme: 'stripe',
+  };
+
   const options = {
-    fetchClientSecret,
+    clientSecret,
+    appearance,
   };
 
   const incrementItemCount = (item) => {
@@ -67,62 +73,67 @@ const Cart = () => {
       { !cart.length ? 
         <Flex justifyContent="center">
           <Button onClick={() => navigate("/order")}>Add to Order</Button></Flex>:
-        <Flex pl="30px" pr="30px" flexDirection={{base: "column-reverse", md: "row"}}>
-          <Box w={{base: "100%", md: "50%"}}>
-          <EmbeddedCheckoutProvider
-            stripe={stripePromise}
-            options={options}
-          >
-            <EmbeddedCheckout />
-          </EmbeddedCheckoutProvider>
-          </Box>
-          <Box w={{base: "100%", md: "50%"}}>
-            {cart.map((c, index) => (
-              <Card
-                mb="15px"
-                key={c.name + index}
-                direction="row"
-                overflow="hidden"
-                variant="outline"
-              >
-                <Image
-                  objectFit="cover"
-                  maxW={{ base: '125px', sm: '200px' }}
-                  src={c.photoUrl}
-                  alt={c.alt}
-                />
-                <Stack>
-                  <CardBody>
-                    <Heading size="md">{c.name}</Heading>
-                    <Text py="2" color="blue">
-                      ${c.price}
-                    </Text>
-                    <Box py="2">
-                      {Object.entries(c.details).map(([key, value]) => (  
-                        <Text key={c.name + key} mr="5px">{key} : {value}</Text>
-                      ))}
-                    </Box>
-                    <HStack maxW='320px'>
-                      { c.count === 1 ?
-                      <IconButton 
-                          variant='ghost'
-                          colorScheme='red'
-                          size="md"
-                          onClick={() => deleteCartItem(c)}
-                          icon={
-                            <Icon as={FaTrash} boxSize={6}/>
-                        }/> : 
-                        <Button onClick={() => decrementItemCount(c)}>-</Button>
-                      }
-                      <input className="countInput" value={c.count} readOnly onChange={() => {}}/>
-                      <Button onClick={() => incrementItemCount(c)} isDisabled={c.count >= 25}>+</Button>
-                    </HStack>
-                  </CardBody>
-                </Stack>
-              </Card>
-            ))}
-          </Box>
-        </Flex>
+          <>
+          { !clientSecret ? 
+            <Flex w="100%" h="100vh" justifyContent="center" mt="30px">
+              <CircularProgress isIndeterminate color='green.300' />
+            </Flex> 
+            :
+            <Flex pl="30px" pr="30px" flexDirection={{base: "column", md: "row"}}>
+              <Box w={{base: "100%", md: "50%"}} mr={{base:0, md: "20px"}}>
+                <Elements options={options} stripe={stripePromise}>
+                    <StripeCheckout />
+                </Elements>
+              </Box>
+              <Box w={{base: "100%", md: "50%"}}>
+                {cart.map((c, index) => (
+                  <Card
+                    mb="15px"
+                    key={c.name + index}
+                    direction="row"
+                    overflow="hidden"
+                    variant="outline"
+                  >
+                    <Image
+                      objectFit="cover"
+                      maxW={{ base: '125px', sm: '200px' }}
+                      src={c.photoUrl}
+                      alt={c.alt}
+                    />
+                    <Stack>
+                      <CardBody>
+                        <Heading size="md">{c.name}</Heading>
+                        <Text py="2" color="blue">
+                          ${c.price}
+                        </Text>
+                        <Box py="2">
+                          {Object.entries(c.details).map(([key, value]) => (  
+                            <Text key={c.name + key} mr="5px">{key} : {value}</Text>
+                          ))}
+                        </Box>
+                        <HStack maxW='320px'>
+                          { c.count === 1 ?
+                          <IconButton 
+                              variant='ghost'
+                              colorScheme='red'
+                              size="md"
+                              onClick={() => deleteCartItem(c)}
+                              icon={
+                                <Icon as={FaTrash} boxSize={6}/>
+                            }/> : 
+                            <Button onClick={() => decrementItemCount(c)}>-</Button>
+                          }
+                          <input className="countInput" value={c.count} readOnly onChange={() => {}}/>
+                          <Button onClick={() => incrementItemCount(c)} isDisabled={c.count >= 25}>+</Button>
+                        </HStack>
+                      </CardBody>
+                    </Stack>
+                  </Card>
+                ))}
+              </Box>
+            </Flex>
+          }
+        </>
       }
     </Fade>
     </>
